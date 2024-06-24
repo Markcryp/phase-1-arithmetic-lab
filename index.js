5c5e7a08565bc23564d0c218d11e78905d13abe0
Mark <mark.njau@student.moringaschool.com>
2024-06-25 01:50:34 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Define num1 and num2 such that their product is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 such that their modulo operations results in 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+//find the maximum number ina set that includes 20
+const max = Math.max(1, 5, 10, 20);
